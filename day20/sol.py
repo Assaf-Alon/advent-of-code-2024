dcb433e61328fee285638d3be4e0f002a7e195a9
Assaf-Alon <alon.assaf@gmail.com>
2024-12-20 12:04:30 +0200

    Day 20

--- day20/sol.py
+++ day20/sol.py
@@ -1,9 +1,6 @@
-from collections import defaultdict
-import itertools
 import os
-from pprint import pprint
 import re
-from typing import DefaultDict, List, Optional, Set, Tuple
+from typing import Dict, List, Optional, Set, Tuple
 import networkx as nx
 
 from common import DIRECTIONS_RDLU, handle_solution, is_in_board, read_input_as_lines, read_input_as_matrix
@@ -12,6 +9,8 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 EXAMPLE_PATH = dir_path + os.sep + "example.txt"
 INPUT_PATH = dir_path + os.sep + "input.txt"
+
+DEBUG = False
 
 
 def is_cell_habbitable(matrix: List[List[str]], row, col):
@@ -33,47 +32,33 @@ def add_valid_edges(G: nx.DiGraph, matrix: List[List[str]]):
 
             for dr, dc in DIRECTIONS_RDLU:
                 if is_cell_habbitable(matrix, row + dr, col + dc):
-                    for is_cheat_available in (True, False):
-                        src, dst = (row, col, is_cheat_available), (row + dr, col + dc, is_cheat_available)
-                        edges.append((src, dst, 1))
+                    src, dst = (row, col), (row + dr, col + dc)
+                    edges.append((src, dst))
 
-    G.add_weighted_edges_from(edges)
+    G.add_edges_from(edges)
 
 
-def get_possible_dsts(
-    start: Tuple[int, int], matrix: List[List[str]], directions: Tuple[int, int], steps: int
-) -> Set[Tuple[int, int]]:
+def get_possible_dsts(start: Tuple[int, int], matrix: List[List[str]], steps: int) -> Set[Tuple[int, int]]:
     dsts = set()
     curr = set()
+    curr.add(start)
+
     row, col = start
-    # First step must actually cheat
-    rows = len(matrix)
-    cols = len(matrix[0])
-    for dr, dc in directions:
-        if is_in_board(rows, cols, row + dr, col + dc) and matrix[row + dr][col + dc] == "#":
-            curr.add((row + dr, col + dc))
 
-    # Maybe for part 2...
-    # for step in range(1, steps - 1):
-    #     dsts_after_step = []
-    #     for (r, c) in curr:
-    #         for (dr, dc) in directions:
-    #             if is_in_board(matrix, r + dr, c + dc)
-
-    # Last step mustn't cheat
-    for r, c in curr:
-        for dr, dc in directions:
-            if is_cell_habbitable(matrix, r + dr, c + dc):
-                dsts.add((r + dr, c + dc))
+    for dr in range(-steps, steps + 1):
+        for dc in range(-steps, steps + 1):
+            if abs(dr) + abs(dc) > steps:
+                continue
+            if not is_cell_habbitable(matrix, row + dr, col + dc):
+                continue
+            dst = row + dr, col + dc
+            dsts.add(dst)
 
     dsts.remove(start)
     return dsts
 
 
-def add_cheating_edges(G: nx.DiGraph, matrix: List[List[str]], max_cheats: int):
-    if max_cheats == 0:
-        return
-
+def get_cheating_edges(matrix: List[List[str]], max_cheats: int) -> List[Tuple[int, int, int]]:
     rows = len(matrix)
     cols = len(matrix[0])
     edges = []
@@ -82,26 +67,32 @@ def add_cheating_edges(G: nx.DiGraph, matrix: List[List[str]], max_cheats: int):
             if not is_cell_habbitable(matrix, row, col):
                 continue
 
-            possible_cheating_dst = get_possible_dsts((row, col), matrix, DIRECTIONS_RDLU, max_cheats)
+            src = (row, col)
+            possible_cheating_dst = get_possible_dsts(src, matrix, max_cheats)
             for dst in possible_cheating_dst:
-                src = (row, col, True)
-                actual_dst = (*dst, False)
-                edges.append((src, actual_dst, max_cheats))
+                man_dist = abs(src[0] - dst[0]) + abs(src[1] - dst[1])
+                edges.append((src, dst, man_dist))
+    if DEBUG:
+        print(len(edges))
+    return edges
 
+
+def add_cheating_edges(G: nx.DiGraph, matrix: List[List[str]], max_cheats: int):
+    if max_cheats == 0:
+        return
+    edges = get_cheating_edges(matrix, max_cheats)
     G.add_weighted_edges_from(edges)
 
 
-def construct_graph_from_board(matrix: List[List[str]], max_cheats: int = 0) -> nx.DiGraph:
+def construct_graph_from_board(matrix: List[List[str]]) -> nx.DiGraph:
     G = nx.DiGraph()
     rows = len(matrix)
     cols = len(matrix[0])
     for row in range(rows):
         for col in range(cols):
-            for is_cheat_available in (True, False):
-                G.add_node((row, col, is_cheat_available))
+            G.add_node((row, col))
 
     add_valid_edges(G, matrix)
-    add_cheating_edges(G, matrix, max_cheats)
     return G
 
 
@@ -121,10 +112,10 @@ def get_start_and_end(matrix: List[List[str]]):
     raise ValueError("Bad matrix!")
 
 
-def get_parsed_input(input_path: str, max_cheats: int):
+def get_parsed_input(input_path: str):
     matrix = read_input_as_matrix(input_path)
     start, end = get_start_and_end(matrix)
-    return construct_graph_from_board(matrix, max_cheats), (*start, True), (*end, max_cheats == 0)
+    return construct_graph_from_board(matrix), start, end
 
 
 def find_cheating_edge_from_path(
@@ -139,53 +130,37 @@ def find_cheating_edge_from_path(
 
 def solve_part1(input_path: str, expected_output: Optional[int] = None, max_cheats: int = 2, min_discount: int = 100):
     sol = 0
-    cheatless_G, start, end = get_parsed_input(input_path, 0)
-    cheatless_shortest_path_length = nx.shortest_path_length(cheatless_G, start, end)
+    matrix = read_input_as_matrix(input_path)
+    cheatless_G, start, end = get_parsed_input(input_path)
+    if DEBUG:
+        print(cheatless_G)
+    shortest_paths_to_end = nx.shortest_path_length(cheatless_G, target=end)
+    shortest_paths_from_start = nx.shortest_path_length(cheatless_G, source=start)
+    shortest_path_length = nx.shortest_path_length(cheatless_G, start, end)
 
-    G, start, end = get_parsed_input(input_path, max_cheats)
-    shortest_path = nx.shortest_path(G, start, end, weight="weight")
-    path_weight = nx.path_weight(G, shortest_path, "weight")
-    print(f"path_weight = {path_weight}")
-    print(f"cheatless_shortest_path_length = {cheatless_shortest_path_length}")
-    while path_weight > 0 and (cheatless_shortest_path_length - path_weight) >= min_discount:
-        sol += 1
-        print(f"Found improvement of {cheatless_shortest_path_length - path_weight}")
-        cheating_edge = find_cheating_edge_from_path(shortest_path)
-        G.remove_edge(*cheating_edge)
-        shortest_path = nx.shortest_path(G, start, end, weight="weight")
-        path_weight = nx.path_weight(G, shortest_path, "weight")
+    cheating_edges = get_cheating_edges(matrix, max_cheats)
+    progress_indicator = 0
+    for i, edge in enumerate(cheating_edges):
+        if DEBUG and i // 100 != progress_indicator:
+            progress_indicator += 1
+            print(f"{i} / {len(cheating_edges)}")
+        edge_src = edge[0]
+        edge_dst = edge[1]
+        weight = shortest_paths_from_start[edge_src] + edge[2] + shortest_paths_to_end[edge_dst]
+        sol += int(shortest_path_length - weight >= min_discount)
 
     handle_solution(sol, expected_output)
 
 
-def solve_part2(input_path: str, expected_output: Optional[int] = None, max_cheats: int = 20, min_discount: int = 100):
-    sol = 0
-    cheatless_G, start, end = get_parsed_input(input_path, 0)
-    cheatless_shortest_paths_to_end = nx.shortest_path(cheatless_G, target=end)
-    cheatless_shortest_path_length = nx.shortest_path_length(cheatless_G, start, end)
-    # print(cheatless_shortest_paths_to_end)
-    print(len(cheatless_shortest_paths_to_end))
-    # G, start, end = get_parsed_input(input_path, max_cheats)
-    # shortest_path = nx.shortest_path(G, start, end, weight="weight")
-    # path_weight = nx.path_weight(G, shortest_path, "weight")
-    # print(f"path_weight = {path_weight}")
-    # print(f"cheatless_shortest_path_length = {cheatless_shortest_path_length}")
-    # while path_weight > 0 and (cheatless_shortest_path_length - path_weight) >= min_discount:
-    #     sol += 1
-    #     print(f"Found improvement of {cheatless_shortest_path_length - path_weight}")
-    #     cheating_edge = find_cheating_edge_from_path(shortest_path)
-    #     G.remove_edge(*cheating_edge)
-    #     shortest_path = nx.shortest_path(G, start, end, weight="weight")
-    #     path_weight = nx.path_weight(G, shortest_path, "weight")
-
-    # handle_solution(sol, expected_output)
+def solve_part2(input_path: str, expected_output: Optional[int] = None, min_discount: int = 100):
+    solve_part1(input_path=input_path, expected_output=expected_output, max_cheats=20, min_discount=min_discount)
 
 
 def day20():
-    # solve_part1(EXAMPLE_PATH, min_discount=1, expected_output=44)
-    # solve_part1(INPUT_PATH, min_discount=1500)
-    # solve_part2(EXAMPLE_PATH, min_discount=1)
-    solve_part2(INPUT_PATH)
+    solve_part1(EXAMPLE_PATH, min_discount=1, expected_output=44)
+    solve_part1(INPUT_PATH, 1518)
+    solve_part2(EXAMPLE_PATH, min_discount=50, expected_output=285)
+    solve_part2(INPUT_PATH, expected_output=1032257)
 
 
 if __name__ == "__main__":
